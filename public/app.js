b373dc3c87517f424b7043019fb331d015d6d953
Hrushikesh-0105 <mhrushikesh0105@gmail.com>
2024-11-21 16:54:10 +0530

    changed the web address

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io("https://chatapp-3fd7.onrender.com")//sends request to connect to server
+const socket = io("https://Hrushikesh-0105.github.io/ChatApp")//sends request to connect to server
 //changed here the port is port of the server
 
 const activity=document.querySelector(".activity")
